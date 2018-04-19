33eb4c6887c1115939c8ece8dc33a81403d806cd
Tod E. Kurt <tod@todbot.com>
2018-04-19 12:27:24 -0700

    show demo of "with" use of re-opening blink1, issues #3, fixed in PR #4

--- blink1_demo/simple_control.py
+++ blink1_demo/simple_control.py
@@ -2,5 +2,22 @@ import time
 from blink1.blink1 import blink1
 
 with blink1() as b1:
-    b1.fade_to_color(100, 'navy')
-    time.sleep(10)
+    b1.fade_to_color(300, 'goldenrod')
+    time.sleep(2)
+
+time.sleep(1)
+
+# notice how blink(1) is turned off and closed
+# outside of the 'with' block
+# you can override this this 'switch_off' flag
+
+with blink1(switch_off=False) as b1:
+    b1.fade_to_color(300, 'pink')
+    time.sleep(2)
+
+with blink1() as b1:
+    b1.fade_to_color(300, 'aqua')
+    time.sleep(2)
+
+    
+
