91ecfdb00226223445411f7dc49916bcfb8a1e5e
Tod E. Kurt <tod@todbot.com>
2018-04-21 19:26:07 -0700

    from blink1-shine, remove white_point arg due to issues on Python2, add fade arg

--- blink1/shine.py
+++ blink1/shine.py
@@ -3,11 +3,11 @@ from blink1.blink1 import blink1
 
 @click.command()
 @click.option('--color', default='white', help='What colour to set the Blink(1)')
-@click.option('--white_point', default='incandescent', help='The name of the white-point to use')
+@click.option('--fade', default=0.2, help='Fade time in seconds')
 
-def shine(color, white_point):
-    with blink1(switch_off=False, white_point=white_point) as b1:
-        b1.fade_to_color(100, color)
+def shine(color,fade):
+    with blink1(switch_off=False) as b1:
+        b1.fade_to_color(fade*1000.0, color)
 
 
 if __name__ == '__main__':
