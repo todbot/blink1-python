b58817116d4cd8b5ae9c2b1bbfda50726f3f6674
Kevin Schlosser <drschlosser@hotmail.com>
2020-08-29 12:02:53 -0600

    pip 8 coding fixes blink1.py

--- blink1/blink1.py
+++ blink1/blink1.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 blink1.py -- blink(1) Python library using python hidapi
 
@@ -12,7 +13,7 @@ from contextlib import contextmanager
 import webcolors
 import hid
 import os
-#from builtins import str as text
+# from builtins import str as text
 
 from .kelvin import kelvin_to_rgb, COLOR_TEMPERATURES
 
@@ -20,6 +21,7 @@ from .kelvin import kelvin_to_rgb, COLOR_TEMPERATURES
 class Blink1ConnectionFailed(RuntimeError):
     """Raised when we cannot connect to a Blink(1)
     """
+
 
 class InvalidColor(ValueError):
     """Raised when the user requests an implausible colour
@@ -40,6 +42,7 @@ PRODUCT_ID = 0x01ed
 
 REPORT_SIZE = 9  # 8 bytes + 1 byte reportId
 
+
 class ColorCorrect(object):
     """Apply a gamma correction to any selected RGB color, see:
     http://en.wikipedia.org/wiki/Gamma_correction
@@ -47,7 +50,8 @@ class ColorCorrect(object):
     def __init__(self, gamma, white_point):
         """
         :param gamma: Tuple of r,g,b gamma values
-        :param white_point: White point expressed as (r,g,b), integer color temperature (in Kelvin) or a string value.
+        :param white_point: White point expressed as (r,g,b), integer color
+            temperature (in Kelvin) or a string value.
 
         All gamma values should be 0 > x >= 1
         """
@@ -57,7 +61,7 @@ class ColorCorrect(object):
         if isinstance(white_point, str):
             kelvin = COLOR_TEMPERATURES[white_point]
             self.white_point = kelvin_to_rgb(kelvin)
-        elif isinstance(white_point,(int,float)):
+        elif isinstance(white_point, (int, float)):
             self.white_point = kelvin_to_rgb(white_point)
         else:
             self.white_point = white_point
@@ -67,11 +71,14 @@ class ColorCorrect(object):
         return round(white * (luminance / 255.0) ** gamma)
 
     def __call__(self, r, g, b):
-        color = [r,g,b]
-        return tuple(self.gamma_correct(g, w, l) for (g, w, l) in zip(self.gamma, self.white_point, color) )
+        color = [r, g, b]
+        return tuple(
+            self.gamma_correct(g, w, l)
+            for (g, w, l) in zip(self.gamma, self.white_point, color)
+        )
 
 
-class Blink1:
+class Blink1(object):
     """Light controller class, sends messages to the blink(1) via USB HID.
     """
     def __init__(self, serial_number=None, gamma=None, white_point=None):
@@ -80,11 +87,11 @@ class Blink1:
         :param gamma: Triple of gammas for each channel e.g. (2, 2, 2)
         """
         self.cc = ColorCorrect(
-            gamma=(gamma or DEFAULT_GAMMA),
+            gamma=gamma or DEFAULT_GAMMA,
             white_point=(white_point or DEFAULT_WHITE_POINT)
         )
         self.dev = self.find(serial_number)
-        if( self.dev is None ):
+        if self.dev is None:
             print("wtf")
 
     def close(self):
@@ -98,16 +105,16 @@ class Blink1:
         :raises: Blink1ConnectionFailed: if blink(1) is not present
         """
         try:
-            hidraw = hid.device(VENDOR_ID,PRODUCT_ID,serial_number)
-            hidraw.open(VENDOR_ID,PRODUCT_ID,serial_number)
+            hidraw = hid.device(VENDOR_ID, PRODUCT_ID, serial_number)
+            hidraw.open(VENDOR_ID, PRODUCT_ID, serial_number)
             # hidraw = hid.device(VENDOR_ID,PRODUCT_ID,unicode(serial_number))
             # hidraw.open(VENDOR_ID,PRODUCT_ID,unicode(serial_number))
         except IOError as e:  # python2
             raise Blink1ConnectionFailed(e)
-            hidraw = None
+            # hidraw = None
         except OSError as e:  # python3
             raise Blink1ConnectionFailed(e)
-            hidraw = None
+            # hidraw = None
 
         return hidraw
 
@@ -117,8 +124,8 @@ class Blink1:
         :return: List of blink(1) device serial numbers
         """
         try:
-            devs = hid.enumerate(VENDOR_ID,PRODUCT_ID)
-            serials = list(map(lambda d:d.get('serial_number'), devs))
+            devs = hid.enumerate(VENDOR_ID, PRODUCT_ID)
+            serials = list(map(lambda d: d.get('serial_number'), devs))
             return serials
         except IOError as e:
             return []
@@ -135,15 +142,15 @@ class Blink1:
         log.debug("blink1write:" + ",".join('0x%02x' % v for v in buf))
         rc = self.dev.send_feature_report(buf)
         # return self.dev.send_feature_report(buf)
-        if( rc != REPORT_SIZE ):
-            raise Blink1ConnectionFailed("write returned %d instead of %d" %(rc,REPORT_SIZE))
+        if rc != REPORT_SIZE:
+            raise Blink1ConnectionFailed("write returned %d instead of %d" % (rc, REPORT_SIZE))
 
     def read(self):
         """ Read command result from blink(1), low-level internal use
         Receive USB Feature Report 0x01 from blink(1) with 8-byte payload
         Note: buf must be 8 bytes or bad things happen
         """
-        buf = self.dev.get_feature_report(REPORT_ID,9)
+        buf = self.dev.get_feature_report(REPORT_ID, 9)
         log.debug("blink1read: " + ",".join('0x%02x' % v for v in buf))
         return buf
 
@@ -156,7 +163,7 @@ class Blink1:
         th = (fade_time & 0xff00) >> 8
         tl = fade_time & 0x00ff
         buf = [REPORT_ID, action, int(red), int(green), int(blue), th, tl, ledn, 0]
-        self.write( buf )
+        self.write(buf)
 
     def fade_to_rgb(self, fade_milliseconds, red, green, blue, ledn=0):
         """ Command blink(1) to fade to RGB color
@@ -188,7 +195,6 @@ class Blink1:
             return webcolors.name_to_rgb(color)
         except ValueError:
             raise InvalidColor(color)
-
 
     def fade_to_color(self, fade_milliseconds, color, ledn=0):
         """ Fade the light to a known colour
@@ -232,24 +238,28 @@ class Blink1:
         :param count: number of times to play, 0=play forever
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
-        if ( self.dev == None ): raise Blink1ConnectionFailed("must open first")
+        if self.dev is None:
+            raise Blink1ConnectionFailed("must open first")
+
         buf = [REPORT_ID, ord('p'), 1, int(start_pos), int(end_pos), int(count), 0, 0, 0]
-        self.write(buf);
+        self.write(buf)
 
     def stop(self):
         """ Stop internal color pattern playing
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
-        if ( self.dev == None ): return False
+        if self.dev is None:
+            return False
+
         buf = [REPORT_ID, ord('p'), 0, 0, 0, 0, 0, 0, 0]
-        self.write(buf);
+        self.write(buf)
 
     def save_pattern(self):
         """ Save internal RAM pattern to flash
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
         buf = [REPORT_ID, ord('W'), 0xBE, 0xEF, 0xCA, 0xFE, 0, 0, 0]
-        self.write(buf);
+        self.write(buf)
 
     def set_ledn(self, ledn=0):
         """ Set the 'current LED' value for writePatternLine
@@ -274,7 +284,7 @@ class Blink1:
         th = (step_time & 0xff00) >> 8
         tl = step_time & 0x00ff
         buf = [REPORT_ID, ord('P'), int(r), int(g), int(b), th,tl, pos, 0]
-        self.write(buf);
+        self.write(buf)
 
     def read_pattern_line(self, pos):
         """ Read a color pattern line at position
@@ -285,28 +295,30 @@ class Blink1:
         buf = [REPORT_ID, ord('R'), 0, 0, 0, 0, 0, int(pos), 0]
         self.write(buf)
         buf = self.read()
-        (r,g,b) = (buf[2],buf[3],buf[4])
+
+        r, g, b = buf[2:4]
+
         step_millis = ((buf[5] << 8) | buf[6]) * 10
-        return (r,g,b,step_millis)
+        return (r, g, b, step_millis)
 
     def read_pattern(self):
         """ Read the entire color pattern
         :return List of pattern line tuples
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
-        pattern=[]
-        for i in range(0,16):    # FIXME: adjustable for diff blink(1) models
-            pattern.append( self.read_pattern_line(i) )
+        pattern = []
+        for i in range(0, 16):    # FIXME: adjustable for diff blink(1) models
+            pattern.append(self.read_pattern_line(i))
         return pattern
 
     def clear_pattern(self):
         """ Clear entire color pattern in blink(1)
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
-        for i in range(0,16):  # FIXME: pattern length
-          rc = write_pattern_line(self.dev, 0, 'black', i );
+        for i in range(0, 16):  # FIXME: pattern length
+          self.write_pattern_line(self.dev, 0, 'black', i)
 
-    def play_pattern(self,pattern_str, onDevice=True):
+    def play_pattern(self, pattern_str, onDevice=True):
         """ Play a Blink1Control-style pattern string
         :param pattern_str: The Blink1Control-style pattern string to play
         :param onDevice: True (default) to run pattern on blink(1),
@@ -314,35 +326,42 @@ class Blink1:
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
         if not onDevice:
-            return self.playPatternLocal(pattern_str)
+            return self.play_pattern_local(pattern_str)
 
         # else, play it in the blink(1)
-        (num_repeats,colorlist) = self.parse_pattern(pattern_str)
+        num_repeats, colorlist = self.parse_pattern(pattern_str)
         pattlen = len(colorlist)
-        if( pattlen < 32 ):      # extend out pattern to fill
-            for i in range(0,32-pattlen):
-                c = {'rgb':'#000000','time':0.0,'ledn':0,'millis':0}
+        if pattlen < 32:      # extend out pattern to fill
+            for i in range(0, 32 - pattlen):
+                c = {
+                    'rgb': '#000000',
+                    'time': 0.0,
+                    'ledn': 0,
+                    'millis': 0
+                }
                 colorlist.append(c)
 
-        for i in range(0,32):
-             c = colorlist[i]
-             self.write_pattern_line( c['millis'], c['rgb'], i, c['ledn'])
+        for i in range(0, 32):
+            c = colorlist[i]
+            self.write_pattern_line(c['millis'], c['rgb'], i, c['ledn'])
 
         return self.play(count=num_repeats)
 
-
-    def play_pattern_local(self,pattern_str):
+    def play_pattern_local(self, pattern_str):
         """ Play a Blink1Control pattern string in Python process
             (plays in blink1-python, so blocks)
         :param pattern_str: The Blink1Control-style pattern string to play
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
-        (num_repeats,colorlist) = self.parse_pattern(pattern_str)
-        if num_repeats == 0: num_repeats = -1;
-        while( num_repeats ):
-            num_repeats = num_repeats - 1
+        num_repeats, colorlist = self.parse_pattern(pattern_str)
+        if num_repeats == 0:
+            num_repeats = -1
+
+        while num_repeats:
+            num_repeats -= 1
+
             for c in colorlist:
-                self.fade_to_color( c['millis'], c['rgb'], c['ledn'])
+                self.fade_to_color(c['millis'], c['rgb'], c['ledn'])
                 time.sleep(c['time'])
 
     @staticmethod
@@ -352,27 +371,31 @@ class Blink1:
         :param pattern_str: The Blink1Control-style pattern string to parse
         :returns: an list of dicts of the parsed out pieces
         """
-        pattparts = pattern_str.replace(' ','').split(',')
-        num_repeats = int(pattparts[0]) # FIXME
+        pattparts = pattern_str.replace(' ', '').split(',')
+        num_repeats = int(pattparts[0])  # FIXME
         pattparts = pattparts[1:]
 
         colorlist = []
-        dpattparts = dict(enumerate(pattparts)) # lets us use .get(i,'default')
-        for i in range(0,len(pattparts),3):
-            rgb =  dpattparts.get(i+0,'#000000')
-            time = float(dpattparts.get(i+1, 0.0))
-            ledn = int(dpattparts.get(i+2, 0))
+        dpattparts = dict(enumerate(pattparts))  # lets us use .get(i,'default')
+        for i in range(0, len(pattparts), 3):
+            rgb = dpattparts.get(i + 0, '#000000')
+            time_ = float(dpattparts.get(i + 1, 0.0))
+            ledn = int(dpattparts.get(i + 2, 0))
             # set default if empty string
-            rgb = rgb if rgb else '#000000' # sigh
-            time = time if time else 0.0 # sigh
-            ledn = ledn if ledn else 0 # sigh
-            millis = int(time * 1000)
-            color = { 'rgb':rgb, 'time':time, 'ledn':ledn, 'millis':millis }
+            rgb = rgb if rgb else '#000000'  # sigh
+            time_ = time_ if time_ else 0.0  # sigh
+            ledn = ledn if ledn else 0  # sigh
+            millis = int(time_ * 1000)
+            color = {
+                'rgb': rgb,
+                'time': time_,
+                'ledn': ledn,
+                'millis': millis
+            }
 
             colorlist.append(color)
 
-        return (num_repeats,colorlist)
-
+        return (num_repeats, colorlist)
 
     def server_tickle(self, enable, timeout_millis=0, stay_lit=False, start_pos=0, end_pos=16):
         """Enable/disable servertickle / serverdown watchdog
@@ -383,12 +406,14 @@ class Blink1:
         :param: end_pos: Sub-pattern end position in whole color pattern
         :raises: Blink1ConnectionFailed: if blink(1) is disconnected
         """
-        if ( self.dev == None ): return ''
-        en = int(enable == True)
-        timeout_time = int(timeout_millis/10)
-        th = (timeout_time & 0xff00) >>8
+        if self.dev is None:
+            return ''
+
+        en = int(enable is True)
+        timeout_time = int(timeout_millis / 10)
+        th = (timeout_time & 0xff00) >> 8
         tl = timeout_time & 0x00ff
-        st = int(stay_lit == True)
+        st = int(stay_lit is True)
         buf = [REPORT_ID, ord('D'), en, th, tl, st, start_pos, end_pos, 0]
         self.write(buf)
 
