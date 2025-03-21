79e2098a93495b733566b08d37aa8ea8fb8fa2bb
Tod Kurt <todbot@users.noreply.github.com>
2025-03-21 15:46:45 -0700

    update hidapi, cleanup

--- setup.py
+++ setup.py
@@ -1,18 +1,11 @@
 from setuptools import setup
-#import os
 
-#PROJECT_ROOT, _ = os.path.split(__file__)
-REVISION = '0.3.1'
+REVISION = '0.3.2'
 PROJECT_NAME = 'blink1'
 PROJECT_AUTHORS = "Salim Fadhley, Tod E. Kurt"
 PROJECT_EMAILS = 'salimfadhley@gmail.com, todbotdotcom@gmail.com'
 PROJECT_URL = "https://github.com/todbot/blink1-python"
 SHORT_DESCRIPTION = 'Official blink(1) control library'
-
-#try:
-#    DESCRIPTION = open(os.path.join(PROJECT_ROOT, "README.md")).read()
-#except IOError:
-#    DESCRIPTION = SHORT_DESCRIPTION
 
 
 setup(
@@ -23,7 +16,7 @@ setup(
     packages=['blink1', 'blink1_tests'],
     zip_safe=True,
     include_package_data=False,
-    install_requires=['hidapi>=0.7.99', 'click', 'webcolors'],
+    install_requires=['hidapi>=0.13.1', 'click', 'webcolors'],  # was hidapi>=0.7.99
     test_suite='nose.collector',
     tests_require=['mock', 'nose', 'coverage'],
     url=PROJECT_URL,
