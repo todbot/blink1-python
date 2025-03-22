2b2f41a597a2d2983504310198cc1e38d4d19d02
Tod Kurt <todbot@users.noreply.github.com>
2025-03-21 17:02:08 -0700

    move from setup.py to pyproject.toml

--- blink1/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-""" blink1 library for python"""
-
-# __all__ = [
-#     'Blink1',
-#     'Blink1ConnectionFailed',
-# ]
-# from .blink1 import Blink1,Blink1ConnectionFailed
